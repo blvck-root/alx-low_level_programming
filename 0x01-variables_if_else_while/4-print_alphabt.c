f98ae08106d72568151988261a30b73d417b4229
blvk-root <mtndlovu81@gmail.com>
2023-09-09 12:28:41 -0700

    Print the alphabet except the letters e and q using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char alphabet[30] = "abcdefghijklmnopqrstuvwxyz\n";
+
+	for (i = 0; i < 27; i++)
+		if (i !=  4 && i != 16)
+			putchar(alphabet[i]);
+
+	return (0);
+}
