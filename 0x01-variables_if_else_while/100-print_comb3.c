882cb3e9589dde4ec95c6d9440f40c8249ea7b76
blvk-root <mtndlovu81@gmail.com>
2023-09-10 06:11:45 -0700

    Print a comma separated list of the smallest combination of 2 unique digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	int i = 0;
+
+	while (i < 10)
+	{
+		j = i + 1;
+		while (j < 10)
+		{
+			if  (j > 1)
+			{
+				putchar(' ');
+				putchar(',');
+			}
+
+			putchar('0' + i);
+			putchar('0' + j);
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
