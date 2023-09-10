327b37be626a0216d65c8d456bae01d1ef5b87bb
blvk-root <mtndlovu81@gmail.com>
2023-09-10 11:31:52 -0700

    Print all the possible combinations of 2 two-digit numbers between 0 and 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * zero_pad - print single-digit and double-digit numbers
+ * as two digit numbers
+ *
+ * Input: 0 - 99 (int)
+ *
+ * Return: 1 if successful, 0 if error
+ */
+int zero_pad(int n)
+{
+	if (n < 10)
+	{
+		putchar('0');
+		putchar('0' + n);
+	}
+	else if (n < 100)
+	{
+		putchar('0' + (n / 10));
+		putchar('0' + (n % 10));
+	}
+	else
+	{
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			if (j > 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+
+			zero_pad(i);
+			putchar(' ');
+			zero_pad(j);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
