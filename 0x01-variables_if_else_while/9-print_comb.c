c72a77f2e56bd11c4e977c2a8f0082129b36951b
blvk-root <mtndlovu81@gmail.com>
2023-09-09 21:59:44 -0700

    Print a comma separated list of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	/* Print single digit numbers of base 10 */
+	while (i < 10)
+	{
+		putchar('0' + i);
+		
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
