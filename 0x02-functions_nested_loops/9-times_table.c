862dd80a6a54fd5df2ac176c96dfdd1a81d287e9
blvk-root <mtndlovu81@gmail.com>
2023-09-12 21:01:05 -0700

    Print times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9-times table
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int res;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			res = i * j;
+
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (res < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + res);
+				}
+				else
+				{
+					_putchar('0' + (res / 10));
+					_putchar('0' + (res % 10));
+				}
+			} 
+			else
+			{
+				_putchar('0');
+			}
+
+		}
+		_putchar('\n');
+	}
+}
