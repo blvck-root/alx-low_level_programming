5a279649db41e1b14818fb094c1672b5698c5ea8
blvk-root <mtndlovu81@gmail.com>
2023-09-15 15:37:49 -0700

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,46 +1,22 @@
 #include <stdio.h>
 
 /**
- * _isprime - determines if an integer is a prime number
- * @n: integer
- * Return: 0 if n is prime 0 otherwise
- */
-long _isprime(long n)
-{
-	long f = 2;
-
-	if (n < 2)
-		return (1);
-
-	if (n == 2)
-		return (0);
-
-	while (f * f < n)
-	{
-		if (n % f == 0)
-			return (1);
-		f++;
-	}
-
-	return (0);
-}
-
-/**
  * main - prints the largest prime factor
  * Return: Always 0
  */
 int main(void)
 {
 	long f = 2;
-	long lpf = 2;
-	long n = 1231952;
+	long n = 612852475143;
 
 	while (f < n)
 	{
-		if ((_isprime(f) == 0) && (n % f == 0))
-			lpf = f;
+		while (n % f == 0)
+		{
+			n /= f;
+		}
 		f++;
 	}
-	printf("%ld\n", lpf);
+	printf("%ld\n", n);
 	return (0);
 }
