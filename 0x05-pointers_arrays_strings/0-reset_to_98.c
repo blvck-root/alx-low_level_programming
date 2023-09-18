6884d61a92d91dad2d493c62316de6fd6339c215
blvck-root <mtndlovu81@gmail.com>
2023-09-18 19:58:13 +0200

    Update the value of an int referenced by a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an int referenced by a pointer
+ * @n: pointer of type int
+ * Return: Void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+#Pointers, Strings, and Arrays
+
+## Task 0
+- Write a function that updates the value of an int referenced by a pointer
