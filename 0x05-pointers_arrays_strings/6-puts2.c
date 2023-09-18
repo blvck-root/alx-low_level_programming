337d7ffcd08f0245592f87da7d1a2cfe42d80756
blvck-root <mtndlovu81@gmail.com>
2023-09-18 22:57:32 +0200

    Prevent accessing out-of-bounds index

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * puts2 - prints every other character of a string
@@ -8,8 +9,9 @@
 void puts2(char *str)
 {
 	int i = 0;
+	int l = strlen(str);
 
-	while (str[i] != '\0')
+	while (i < l)
 	{
 		_putchar(str[i]);
 		i += 2;
