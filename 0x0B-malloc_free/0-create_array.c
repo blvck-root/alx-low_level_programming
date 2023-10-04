92be4c390d768667aeaf8eade262fd70609e6938
blvck-root <mtndlovu81@gmail.com>
2023-10-04 19:58:19 +0200

    updated create_array function to check if malloc is successful

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 
-	if (size == 0)
-		return (NULL);
-
-	s = malloc(size * sizeof(c));
 	i = 0;
+	s = malloc(size * sizeof(c));
+
+	if (size == 0 || s == NULL)
+		return (NULL);
 
 	while (i < size)
 		s[i++] = c;
