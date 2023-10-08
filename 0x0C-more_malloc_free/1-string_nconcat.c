976f833c36b77da72fc40d8f9b209c22e0d15f93
blvck-root <mtndlovu81@gmail.com>
2023-10-08 22:30:37 +0200

    remove code that uses standard library functions

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
 /**
@@ -13,10 +12,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
-	unsigned int len1 = strlen(s1);
-	unsigned int len2 = strlen(s2);
-	unsigned int n1 = n < len2 ? n : len2;
-	char *s = malloc(len1 + n1 + 1);
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int n1;
+	char *s;
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+
+	n1 = n < len2 ? n : len2;
+	s = malloc(len1 + n1 + 1);
 
 	if (s == NULL)
 	{
