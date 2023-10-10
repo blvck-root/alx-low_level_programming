8288e88d83e2c08e5bc6a1f6fc11210b2ddf4ee4
blvck-root <mtndlovu81@gmail.com>
2023-10-11 01:40:58 +0200

    add comments for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
 #ifndef HEADERFILE_H
 #define HEADERFILE_H
 
-typedef struct dog dog_t;
-struct dog {
+/**
+ * dog - definition of dog struct
+ */
+typedef struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
