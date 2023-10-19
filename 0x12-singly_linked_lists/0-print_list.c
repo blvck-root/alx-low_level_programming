08a329419ef736eb55d8b3778022eab14cd59cac
blvck-root <mtndlovu81@gmail.com>
2023-10-19 11:28:31 +0200

    add README.md and print [0] (nil) when str is NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 		if (h->str)
 			printf("[%u] %s\n", h->len, h->str);
 		else
-			printf("[0] (null)\n");
+			printf("[0] (nil)\n");
 		h = h->next;
 		++i;
 	}
