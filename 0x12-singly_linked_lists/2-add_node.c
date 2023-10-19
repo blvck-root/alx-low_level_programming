65d42beffa9054346b492332d71e4fd3fd2cae73
blvck-root <mtndlovu81@gmail.com>
2023-10-19 14:34:51 +0200

    refactor add_node function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,30 +1,29 @@
 #include "lists.h"
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-int _strlen(char *str);
+int _strlen(const char *str);
 
 /**
  * add_node - add node to the start of a linkedlist
  * @head: pointer to linkedlist
+ * @str: string
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	char *s = strdup(str);
 	list_t *new;
 
-	if (head == NULL || s == NULL)
+	if (head == NULL || str == NULL)
 		return (NULL);
 
 	new = (list_t *) malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
-	new->str = s;
-	new->len = _strlen(s);
+	new->str = strdup(str);
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 	return (new);
@@ -35,7 +34,7 @@ list_t *add_node(list_t **head, const char *str)
  * @str: string
  * Return: number of characters in a string
  */
-int _strlen(char *str)
+int _strlen(const char *str)
 {
 	int i = 0;
 
