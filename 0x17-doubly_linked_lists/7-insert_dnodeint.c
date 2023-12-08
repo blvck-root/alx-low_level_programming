9108c7714e405f8aafb25970873d5b19635227c5
blvck-root <mtndlovu81@gmail.com>
2023-12-08 11:58:31 +0200

    remove debug code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node = node->next;
 		--idx;
 	}
-	printf("idx-%u\n", idx);
 	if (idx == 1)
 		return (add_dnodeint_end(&node, n));
 	return (NULL);
